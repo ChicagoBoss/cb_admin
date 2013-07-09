328adbc7dbd1b49fdcc588e986b6c7e1cafee8b5
Eric Cestari <ecestari@mac.com>
2013-07-09 10:20:21 +0200

    latest version of boss_plugin, find boss without specifying path

--- priv/rebar/boss_plugin.erl
+++ priv/rebar/boss_plugin.erl
@@ -7,8 +7,8 @@
 -module(boss_plugin).
 
 -export([boss/2,
-		 pre_compile/2,
-		 pre_eunit/2]).
+     pre_compile/2,
+     pre_eunit/2]).
 
 -define(BOSS_PLUGIN_CLIENT_VERSION, 1).
 -define(BOSS_CONFIG_FILE, "boss.config").
@@ -26,22 +26,22 @@
 %% @end
 %%--------------------------------------------------------------------
 boss(RebarConf, AppFile) ->
-	case is_base_dir(RebarConf) of
-		true -> 
+  case is_base_dir(RebarConf) of
+    true -> 
             Command = rebar_config:get_global(RebarConf, c, "help"),
-			{ok, BossConf} = init(RebarConf, AppFile, Command),
-			case boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, Command, RebarConf, BossConf, AppFile) of
-				{error, command_not_found} ->
-					io:format("ERROR: boss command not found.~n"), 
-					boss_rebar:help(),
-					halt(1);
-				{error, Reason} -> 
-					io:format("ERROR: executing ~s task: ~s~n", [Command, Reason]), 
-					halt(1);
-				ok -> ok
-			end;
-		false -> ok
-	end.
+      {ok, BossConf} = init(RebarConf, AppFile, Command),
+      case boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, Command, RebarConf, BossConf, AppFile) of
+        {error, command_not_found} ->
+          io:format("ERROR: boss command not found.~n"), 
+          boss_rebar:help(),
+          halt(1);
+        {error, Reason} -> 
+          io:format("ERROR: executing ~s task: ~s~n", [Command, Reason]), 
+          halt(1);
+        ok -> ok
+      end;
+    false -> ok
+  end.
 
 %%--------------------------------------------------------------------
 %% @doc initializes the rebar boss connector plugin
@@ -51,49 +51,48 @@ boss(RebarConf, AppFile) ->
 %%--------------------------------------------------------------------
 init(RebarConf, AppFile, Command) ->
     %% Compile and load the boss_rebar code, this can't be compiled
-	%% as a normal boss lib without the rebar source dep
-	%% The load of ./rebar boss:
-	%% - Rebar itself searchs in rebar.config for {plugin_dir, ["priv/rebar"]}.
-	%% - Rebar itself compile this plugin and adds it to the execution chain
-	%% - This plugin compiles and loads the boss_rebar code in ["cb/priv/rebar"],
-	%%   so we can extend/bugfix/tweak the framework without the need of manually
-	%%   recopy code to user apps
+  %% as a normal boss lib without the rebar source dep
+  %% The load of ./rebar boss:
+  %% - Rebar itself searchs in rebar.config for {plugin_dir, ["priv/rebar"]}.
+  %% - Rebar itself compile this plugin and adds it to the execution chain
+  %% - This plugin compiles and loads the boss_rebar code in ["cb/priv/rebar"],
+  %%   so we can extend/bugfix/tweak the framework without the need of manually
+  %%   recopy code to user apps
     {BossConf, BossConfFile} = boss_config(Command),
-	BossPath = case boss_config_value(boss, path, BossConf) of
-				   {error, _} ->
-					   io:format("FATAL: Failed to read boss=>path config in ~p.~n", [BossConfFile]),
-					   halt(1);
-				   Val -> Val
-			   end,
-	RebarErls = rebar_utils:find_files(filename:join([BossPath, "priv", "rebar"]), ".*\\.erl\$"),
-	
-	rebar_log:log(debug, "Auto-loading boss rebar modules ~p~n", [RebarErls]),
-	
-	lists:map(fun(F) ->
-					  case compile:file(F, [binary]) of
-						  error ->
-							  io:format("FATAL: Failed compilation of ~s module~n", [F]),
-							  halt(1);
-						  {ok, M, Bin} ->
-							  {module, _} = code:load_binary(M, F, Bin),
-							  rebar_log:log(debug, "Loaded ~s~n", [M])
-					  end
-			  end, RebarErls),
+  BossPath = case boss_config_value(boss, path, BossConf) of
+           {error, _} ->
+             filename:join([rebar_config:get_xconf(RebarConf, base_dir, undefined), "deps", "boss"]);
+           Val -> Val
+         end,
+  RebarErls = rebar_utils:find_files(filename:join([BossPath, "priv", "rebar"]), ".*\\.erl\$"),
+  
+  rebar_log:log(debug, "Auto-loading boss rebar modules ~p~n", [RebarErls]),
+  
+  lists:map(fun(F) ->
+            case compile:file(F, [binary]) of
+              error ->
+                io:format("FATAL: Failed compilation of ~s module~n", [F]),
+                halt(1);
+              {ok, M, Bin} ->
+                {module, _} = code:load_binary(M, F, Bin),
+                rebar_log:log(debug, "Loaded ~s~n", [M])
+            end
+        end, RebarErls),
 
-	%% add all cb_apps defined in config file to code path
-	%% including the deps ebin dirs
-	[code:add_path(CodePath) || CodePath <- boss_rebar:all_ebin_dirs(BossConf, AppFile)],
+  %% add all cb_apps defined in config file to code path
+  %% including the deps ebin dirs
+  
   case is_base_dir(RebarConf) of
   true ->
-    ok;
+    code:add_paths(["ebin" | filelib:wildcard(rebar_config:get_xconf(RebarConf, base_dir, undefined) ++ "/deps/*/ebin")]);
   false ->
     code:add_paths(filelib:wildcard(rebar_utils:get_cwd() ++ "/../*/ebin"))
   end,
   %% Load the config in the environment
   boss_rebar:init_conf(BossConf),
-	
-	{ok, BossConf}.
-	
+  
+  {ok, BossConf}.
+  
 %%--------------------------------------------------------------------
 %% @doc pre_compile hook
 %% @spec pre_compile(_Config, AppFile) -> ok | {error, Reason}
@@ -104,13 +103,13 @@ init(RebarConf, AppFile, Command) ->
 %% @end
 %%--------------------------------------------------------------------
 pre_compile(RebarConf, AppFile) ->
-	case is_boss_app(AppFile)  orelse is_base_dir(RebarConf) of
-		true -> 
-			{ok, BossConf} = init(RebarConf, AppFile, "compile"),
-			boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, compile, RebarConf, BossConf, AppFile),
-			halt(0);
-		false -> ok
-	end.
+  case is_boss_app(AppFile)  orelse is_base_dir(RebarConf) of
+    true -> 
+      {ok, BossConf} = init(RebarConf, AppFile, "compile"),
+      boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, compile, RebarConf, BossConf, AppFile),
+      halt(0);
+    false -> ok
+  end.
 
 %%--------------------------------------------------------------------
 %% @doc pre_eunit hook
@@ -122,13 +121,13 @@ pre_compile(RebarConf, AppFile) ->
 %% @end
 %%--------------------------------------------------------------------
 pre_eunit(RebarConf, AppFile) ->
-	case is_boss_app(AppFile) orelse is_base_dir(RebarConf) of
-		true -> 
-			{ok, BossConf} = init(RebarConf, AppFile, "test_eunit"),
-			boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, test_eunit, RebarConf, BossConf, AppFile),
-			halt(0);
-		false -> ok
-	end.
+  case is_boss_app(AppFile) orelse is_base_dir(RebarConf) of
+    true -> 
+      {ok, BossConf} = init(RebarConf, AppFile, "test_eunit"),
+      boss_rebar:run(?BOSS_PLUGIN_CLIENT_VERSION, test_eunit, RebarConf, BossConf, AppFile),
+      halt(0);
+    false -> ok
+  end.
 
 %% ===================================================================
 %% Internal functions
@@ -169,14 +168,14 @@ boss_config(Command) ->
 %% @end
 %%--------------------------------------------------------------------
 boss_config_value(App, Key, BossConfig) ->
-	case lists:keyfind(App, 1, BossConfig) of
-		false -> 
-			{error, boss_config_app_not_found};
-		{App, AppConfig} -> 
-			case lists:keyfind(Key, 1, AppConfig) of
-				false -> 
-					{error, boss_config_app_setting_not_found};
-				{Key, KeyConfig} ->
-					KeyConfig
-			end
-	end.
+  case lists:keyfind(App, 1, BossConfig) of
+    false -> 
+      {error, boss_config_app_not_found};
+    {App, AppConfig} -> 
+      case lists:keyfind(Key, 1, AppConfig) of
+        false -> 
+          {error, boss_config_app_setting_not_found};
+        {Key, KeyConfig} ->
+          KeyConfig
+      end
+  end.
